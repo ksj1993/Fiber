230ad457caa0dd54a4400a2516edb44fe418c1f4
ksj1993 <kunaljasty@gmail.com>
2015-11-15 19:58:16 +0000

    Tornado wsgi

--- webserver/run.py
+++ webserver/run.py
@@ -1,17 +1,25 @@
 from app import app
+from tornado.wsgi import WSGIContainer
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop
+
 
 #run server on port 8111
 if __name__ == "__main__":
     import click
 
-    @click.command()
-    @click.option('--debug', is_flag=True)
-    @click.option('--threaded', is_flag=True)
-    @click.argument('HOST', default='0.0.0.0')
-    @click.argument('PORT', default=8111, type=int)
-    def run(debug, threaded, host, port):
-        HOST, PORT = host, port
-        print "running on %s:%d" % (HOST, PORT)
-        app.run(host=HOST, port=PORT, debug=debug, threaded=threaded)
+#    @click.command()
+#    @click.option('--debug', is_flag=True)
+#    @click.option('--threaded', is_flag=True)
+#    @click.argument('HOST', default='0.0.0.0')
+#    @click.argument('PORT', default=8111, type=int)
+#    def run(debug, threaded, host, port):
+#        HOST, PORT = host, port
+#        print "running on %s:%d" % (HOST, PORT)
+#        app.run(host=HOST, port=PORT, debug=debug, threaded=threaded)
+#
+#    run()
 
-    run()
+    http_server = HTTPServer(WSGIContainer(app))
+    http_server.listen(8111)
+    IOLoop.instance().start()
